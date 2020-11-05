327755871f35a97db404cc5fc414619b9dcfcb8f
luis_gonzales <luis.gonzalez@airbnb.com>
2020-11-04 16:58:05 -0800

    Updated to latest next

--- pages/menuitems.tsx
+++ pages/menuitems.tsx
@@ -1,17 +1,30 @@
+import { useEffect, useState } from 'react';
+import { parameters } from '../lib/auth/getMsalAuthConfig';
 import { useSession } from '../lib/auth/MsalAuth/client';
 
 export const MenuITems = (): JSX.Element => {
-  const { isAuthenticated, isReady } = useSession();
+  const [token, setToken] = useState('');
+  const { isAuthenticated, isReady, getAccessToken } = useSession();
+  useEffect(() => {
+    const getToken = async () => {
+      const t = await getAccessToken(parameters.scopes);
+      setToken(t);
+    };
+    if (isReady) getToken();
+  }, [isReady]);
+
   if (!isReady) {
     return <div>Loading Auth</div>;
   }
   if (!isAuthenticated) {
     return <div>No Access</div>;
   }
+
   return (
     <div className="container">
       <main>
         <h1 className="title">Menu Items</h1>
+        <div>Token {token}</div>
       </main>
     </div>
   );
